5cd1da11421b3fe67cdcfe3f0669e85508799ce4
Amit Ranjan <amitalable@gmail.com>
2021-10-27 21:17:40 +0530

    SortColors

---
+++ Leetcode-October-2021/Week-4/SortColors.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/sort-colors/
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        # Position where next found '0' value will be moved
+        nextZeroPos = 0
+        # Position where next found '2' value will be moved
+        nextTwoPos = len(nums) - 1
+        # Index of currently checking element
+        i = 0
+        while i <= nextTwoPos:
+            if nums[i] == 0:
+                nums[i], nums[nextZeroPos] = nums[nextZeroPos], nums[i]
+                nextZeroPos += 1
+            elif nums[i] == 2:
+                # Next line brings some new value from nextTwoPos to i'th position
+                nums[i], nums[nextTwoPos] = nums[nextTwoPos], nums[i]
+                nextTwoPos -= 1
+                # That's why continue is here: to prevent i(ndex) incremention.
+                continue
+            i += 1
